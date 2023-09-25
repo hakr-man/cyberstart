2d1b9fce40bb061d4d0f16c632b6aa062cbd5a14
David Joslin <58665572+hakr-man@users.noreply.github.com>
2023-09-25 19:47:12 +0000

    rewrote without requests module

--- moonBase/L8/selfDestruct/main.py
+++ moonBase/L8/selfDestruct/main.py
@@ -4,16 +4,16 @@
 # You can easily run out of execution time in this challenge.
 # You will need to check the response and stop your attack
 # once you see the flag.
-import requests
+import urllib.request
 
 url = "http://127.0.0.1:8082/selfdestruct"
 
 while True:
     # if response contains "please try again" then continue
-    response = requests.get(url, timeout=5)
-    if "please try again" in response.text:
+    response = urllib.request.urlopen(url, timeout=5)
+    if "try again".encode() in response.read():
         continue
     else:
-        print(response.text)
+        print(response.read().decode())
         break
     
