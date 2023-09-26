b2ad3fa90463c0cb092d2be2c5a5ac68f562f3c5
David Joslin <58665572+hakr-man@users.noreply.github.com>
2023-09-26 14:49:02 +0000

    completed L08:C05 - Self Destruct

--- moonBase/L8/selfDestruct/main.py
+++ moonBase/L8/selfDestruct/main.py
@@ -4,16 +4,20 @@
 # You can easily run out of execution time in this challenge.
 # You will need to check the response and stop your attack
 # once you see the flag.
+
 import urllib.request
 
 url = "http://127.0.0.1:8082/selfdestruct"
 
 while True:
-    # if response contains "please try again" then continue
-    response = urllib.request.urlopen(url, timeout=5)
-    if "try again".encode() in response.read():
+    # if response contains "try again" then continue
+    response = urllib.request.urlopen(url).read()
+    #print(response)
+    if "try again".encode() in response:
+        print("fail")
         continue
     else:
-        print(response.read().decode())
+        print("success")
+        print(response)
         break
     
